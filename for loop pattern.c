e65036a5ef183abf24419b25049c6af6ff13d54a
Ramon Paulo Antonio Caumban <89231021+ramonPL14@users.noreply.github.com>
2021-11-17 05:43:38 +0800

    Add files via upload

---
+++ for loop pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int a, b;
+
+  for(a=1; a<10; a++)
+  {
+    for(b=a; b<10; b++)
+    {
+      printf("%d", b);
+    }
+    printf("\n");
+    }
+    }
+     
+  
